cfc15687a81fc4e5972e86e342c2bfd6ee943e46
Student <student@Makerss-MacBook-Pro-5.local>
2019-05-02 17:03:07 +0100

    add error message if dock is full

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,25 +4,32 @@ describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 
   describe '#release_bike' do
-  it "releases working bikes" do
+    it "releases working bikes" do
     # bike = subject.release_bike
     # expect(bike.working?).to eq true
     bike = Bike.new
     subject.dock(bike)
     expect(subject.release_bike).to eq bike
   end
-  it 'raises an error when there are no bikes' do
+    it 'raises an error when there are no bikes' do
     expect {subject.release_bike}.to raise_error 'No bikes available'
   end
+
 end
 
   it {is_expected.to respond_to(:dock).with(1).argument}
 
   it {is_expected.to respond_to(:bike)}
 
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+  describe '#dock' do
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+  end
+    it 'raises an error if there is more than 1 bike' do
+      subject.dock(Bike.new)
+      expect {subject.dock Bike.new}.to raise_error 'Docking station is full'
+    end
   end
 
   it 'returns docked bikes' do
